624c4224a02ed51c49e4606e5352e0c2c40d49b7
hieunph <hieunph@vnpt.vn>
2025-05-20 12:38:25 +0700

    cty

--- src/app/pages/property-consignment/property-consignment.component.ts
+++ src/app/pages/property-consignment/property-consignment.component.ts
@@ -112,7 +112,7 @@ export class PropertyConsignmentComponent implements OnInit {
           let formSale = new FormData();
           // Thông tin chủ sở hữu
           formSale.append(
-            'entry.1361929624',
+            'entry.1635723793',
             this.consignmentForm.value.ownerInfo.fullName
           );
           formSale.append(
@@ -169,10 +169,29 @@ export class PropertyConsignmentComponent implements OnInit {
             'entry.278178518',
             this.consignmentForm.value.priceInfo.bankSupport ? 'Có' : 'Không'
           );
+
+          // Log form data trước khi gửi
+          const formObject: any = {};
+          formSale.forEach((value, key) => {
+            formObject[key] = value;
+          });
+          console.log('Form Data:', formObject);
+
+          // Chuyển đổi FormData sang URLSearchParams
+          const params = new URLSearchParams();
+          Object.keys(formObject).forEach(key => {
+            params.append(key, formObject[key]);
+          });
+
           this.http
             .post(
-              'https://docs.google.com/forms/d/e/1FAIpQLSfJWW50QDcK6S2SCn8EWAKhRlHMeg91k-ZDQsap7Wp3bF84XQ/formResponse',
-              formSale
+              'https://docs.google.com/forms/d/e/1FAIpQLScsU-6o1aWHhykPBSlK-lgEYU5hycthJ0INLiBOcxED0llGng/formResponse',
+              params.toString(),
+              {
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                },
+              }
             )
             .subscribe({
               next: () => {
@@ -183,7 +202,9 @@ export class PropertyConsignmentComponent implements OnInit {
                 );
                 this.resetForm();
               },
-              error: () => {
+              error: (error) => {
+                // Log lỗi để debug
+                console.log('Error submitting form:', error);
                 // Google Form luôn trả về lỗi CORS nhưng vẫn submit thành công
                 this.isSubmitting = false;
                 this.toastr.success(
@@ -227,12 +248,8 @@ export class PropertyConsignmentComponent implements OnInit {
             'entry.276637145',
             this.consignmentForm.value.propertyInfo.direction || ''
           );
-          formRental.append(
-            'entry.1526042079',
-            this.consignmentForm.value.propertyInfo.status
-          );
 
-          // Thông tin cho thuê
+          // Thông tin giá và điều kiện
           formRental.append(
             'entry.1283114364',
             this.consignmentForm.value.rentalInfo.price?.toString() || ''
@@ -250,38 +267,92 @@ export class PropertyConsignmentComponent implements OnInit {
             this.consignmentForm.value.rentalInfo.minContract?.toString() || ''
           );
 
+          formRental.append(
+            'entry.494766771',
+            this.consignmentForm.value.rentalInfo.negotiable ? 'Có' : 'Không'
+          );
+
+          // Ngày bàn giao
+          const availableDate = this.consignmentForm.value.rentalInfo.availableDate;
+          if (availableDate) {
+            const date = new Date(availableDate);
+            formRental.append('entry.527403809_year', date.getFullYear().toString());
+            formRental.append('entry.527403809_month', (date.getMonth() + 1).toString());
+            formRental.append('entry.527403809_day', date.getDate().toString());
+          } else {
+            formRental.append('entry.527403809_year', '');
+            formRental.append('entry.527403809_month', '');
+            formRental.append('entry.527403809_day', '');
+          }
+
+          // Trạng thái
+          formRental.append(
+            'entry.1526042079',
+            this.consignmentForm.value.propertyInfo.status
+          );
+
           // Dịch vụ bao gồm
-          const includedServices =
-            this.consignmentForm.value.rentalInfo.includedServices;
-          const services = [];
-          if (includedServices.managementFee) services.push('Phí quản lý');
-          if (includedServices.internet) services.push('Internet');
-          if (includedServices.utilities) services.push('Điện/nước');
-          if (includedServices.parking) services.push('Gửi xe');
-          if (includedServices.other && includedServices.otherDetail)
-            services.push(includedServices.otherDetail);
-          formRental.append('entry.1020586234', services.join(', '));
+          const includedServices = this.consignmentForm.value.rentalInfo.includedServices;
+          if (includedServices) {
+            if (includedServices.managementFee) {
+              formRental.append('entry.1020586234', 'Phí quản lý');
+            }
+            if (includedServices.internet) {
+              formRental.append('entry.1020586234', 'Internet');
+            }
+            if (includedServices.utilities) {
+              formRental.append('entry.1020586234', 'Điện/nước');
+            }
+            if (includedServices.parking) {
+              formRental.append('entry.1020586234', 'Gửi xe');
+            }
+            if (includedServices.other && includedServices.otherDetail) {
+              formRental.append('entry.1020586234', includedServices.otherDetail);
+            }
+          }
 
           // Đối tượng phù hợp
           const suitableFor = this.consignmentForm.value.rentalInfo.suitableFor;
-          const tenants = [];
-          if (suitableFor.family) tenants.push('Gia đình');
-          if (suitableFor.worker) tenants.push('Người đi làm');
-          if (suitableFor.student) tenants.push('Sinh viên');
-          if (suitableFor.foreigner) tenants.push('Người nước ngoài');
-          if (suitableFor.other && suitableFor.otherDetail)
-            tenants.push(suitableFor.otherDetail);
-          formRental.append('entry.1205154710', tenants.join(', '));
+          if (suitableFor) {
+            if (suitableFor.family) {
+              formRental.append('entry.1205154710', 'Gia đình');
+            }
+            if (suitableFor.worker) {
+              formRental.append('entry.1205154710', 'Người đi làm');
+            }
+            if (suitableFor.student) {
+              formRental.append('entry.1205154710', 'Sinh viên');
+            }
+            if (suitableFor.foreigner) {
+              formRental.append('entry.1205154710', 'Người nước ngoài');
+            }
+            if (suitableFor.other && suitableFor.otherDetail) {
+              formRental.append('entry.1205154710', suitableFor.otherDetail);
+            }
+          }
 
-          // Thời gian bàn giao
-          formRental.append(
-            'entry.527403809',
-            this.consignmentForm.value.rentalInfo.availableDate || ''
-          );
+          // Log form data trước khi gửi
+          const formObject: any = {};
+          formRental.forEach((value, key) => {
+            formObject[key] = value;
+          });
+          console.log('Form Data:', formObject);
+
+          // Chuyển đổi FormData sang URLSearchParams
+          const params = new URLSearchParams();
+          Object.keys(formObject).forEach(key => {
+            params.append(key, formObject[key]);
+          });
+
           this.http
             .post(
-              'https://docs.google.com/forms/d/e/1nsKCWMvL8iYss-jmpVZoIJrdU4YZvXSTieXKA9TjUc0/formResponse',
-              formRental
+              'https://docs.google.com/forms/d/e/1FAIpQLSfoJrcqHJ46uqgEg_RqTbxWsSIzXr0Ff77FShraiMJeVSxmrA/formResponse',
+              params.toString(),
+              {
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                },
+              }
             )
             .subscribe({
               next: () => {
@@ -293,8 +364,13 @@ export class PropertyConsignmentComponent implements OnInit {
                 this.resetForm();
               },
               error: () => {
+                // Google Form luôn trả về lỗi CORS nhưng vẫn submit thành công
                 this.isSubmitting = false;
-                this.toastr.error('Gửi thông tin thất bại. Vui lòng thử lại.');
+                this.toastr.success(
+                  'Gửi thông tin thành công! Chúng tôi sẽ liên hệ với bạn sớm.',
+                  'Thành công'
+                );
+                this.resetForm();
               },
             });
         }
